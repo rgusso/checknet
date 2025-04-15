7685bca1faa33e2b951d1683a690f57d443627bc
Rodrigo <rodrigo@metarsul.com.br>
2025-04-15 17:55:43 -0300

    setup.py to english

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="checknet",
     version="0.1.0",
-    description="Verifica se há conexão com a internet usando sockets",
+    description="Check internet connectivity using sockets",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Rodrigo Gusso",
